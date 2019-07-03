5bb89805e5898932de6538166ba19e38a48b7690
yongbingao <ben.gao25@gmail.com>
2019-07-03 09:59:20 -0400

    Add solution to problem 4

--- stack_queue_interview_problems/lib/4_iterate_across_linked_list_backwards.js
+++ stack_queue_interview_problems/lib/4_iterate_across_linked_list_backwards.js
@@ -29,6 +29,69 @@
 function iterateAcrossLinkedListBackwards(linkedList) {
     // TODO: Implement the iterateAcrossLinkedListBackwards function here
 
+    if(linkedList.head === null) return "";
+    else if(linkedList.head === linkedList.tail) return linkedList.head.value.toString();
+    class Node{
+        constructor(val){
+            this.value = val;
+            this.next = null;
+        }
+    }
+
+    class Stack{
+        constructor(){
+            this.top = null;
+            this.bottom = null;
+            this.length = 0;
+        }
+
+        push(val){
+            const newNode = new Node(val);
+            if(this.top === null){
+                this.top = newNode;
+                this.bottom = newNode;
+            } else {
+                newNode.next = this.top;
+                this.top = newNode;
+            }
+
+            return ++this.length;
+        }
+
+        pop(){
+            if(this.top === null) return null;
+            
+            const top = this.top;
+
+            if(this.top === this.bottom){
+                this.top = null;
+                this.bottom = null;
+            } else {
+                this.top = this.top.next;
+            }
+
+            this.length--;
+            return top.value;
+        }
+
+        size(){
+            return this.length;
+        }
+    }
+
+    let currentNode = linkedList.head;
+    const stack = new Stack();
+
+    while(currentNode !== null){
+        stack.push(currentNode.value);
+        currentNode = currentNode.next;
+    }
+
+    let result = "".concat(stack.pop());
+    while(stack.length){
+        result = result.concat(" -> ", stack.pop());
+    }
+    return result;
 }
 
 exports.iterateAcrossLinkedListBackwards = iterateAcrossLinkedListBackwards;
