b1131f0edc5ac7983a0b61dc1d0158fc03a67e8a
yongbingao <ben.gao25@gmail.com>
2019-07-03 00:04:39 -0400

    Add stack_queue solution

--- stack_queue_interview_problems/lib/3_stack_queue.js
+++ stack_queue_interview_problems/lib/3_stack_queue.js
@@ -26,17 +26,104 @@
 
 class Node {
     // TODO: Implement the Node class!
-
+    constructor(val){
+        this.value = val;
+        this.next = null;
+    }
 }
 
 class Stack {
     // TODO: Implement the Stack class!
+    constructor(){
+        this.top = null;
+        this.bottom = null;
+        this.length = 0;
+    }
 
+    push(node){
+        // const newNode = new Node(val);
+        if(this.top === null){
+            this.top = node;
+            this.bottom = node;
+        } else {
+            node.next = this.top;
+            this.top = node;
+        }
+
+        return ++this.length;
+    }
+
+    pop() {
+        if(this.top === null) return null;
+
+        const top = this.top;
+        if(this.top === this.bottom){
+            this.top = null;
+            this.bottom = null;
+        } else {
+            this.top = this.top.next;
+        }
+
+        this.length--;
+        return top;
+    }
+
+    size(){
+        return this.length;
+    }
 }
-
 class StackQueue {
     // TODO: Implement the StackQueue class!
+    constructor(){
+        this.inStack = new Stack();
+        this.outStack = new Stack();
+        this.front = null;
+        this.back = null;
+        this.length = 0;
+    }
 
+    enqueue(val){
+        const newNode = new Node(val);
+
+        if(this.front === null) {
+            this.inStack.push(newNode);
+            this.front = this.inStack.bottom;
+            this.back = this.inStack.top;
+        }
+        else if(this.length === 1){
+            this.front.next = newNode;
+            this.inStack.push(newNode);
+            this.back = this.inStack.top;
+        }
+        else{
+            this.inStack.push(newNode);
+            this.back = this.inStack.top;
+        }
+        return ++this.length;
+    }
+
+    dequeue(){
+        let first;
+        if(this.outStack.top !== null){
+            first = this.outStack.pop();
+            this.front = this.outStack.top || this.inStack.bottom;
+            this.back = this.inStack.top;
+        } else{
+            while(this.inStack.top !== null){
+                this.outStack.push(this.inStack.pop());
+            }
+            first = this.outStack.pop();
+            this.front = this.outStack.top;
+            this.back = this.inStack.top;
+        }
+
+        if (first) this.length--;
+        return first;
+    }
+
+    size(){
+        return this.length;
+    }
 };
 
 exports.Node = Node;
