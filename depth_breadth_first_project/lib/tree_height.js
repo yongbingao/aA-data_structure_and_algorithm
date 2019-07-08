bd7a86a9b43dac5ea4021869ab8c60774f72bc59
yongbingao <ben.gao25@gmail.com>
2019-07-08 16:32:51 -0400

    Add solutions to problems

--- depth_breadth_first_project/lib/tree_height.js
+++ depth_breadth_first_project/lib/tree_height.js
@@ -1,5 +1,11 @@
 function treeHeight(root) {
+    if(root === null) return -1;
+    let height = 0;
 
+    const left = root.left ? treeHeight(root.left)+1 : 0;
+    const right = root.right ? treeHeight(root.right)+1 : 0;
+
+    return height + Math.max(left, right);
 }
 
 
--- depth_breadth_first_project/lib/tree_sum.js
+++ depth_breadth_first_project/lib/tree_sum.js
@@ -1,5 +1,16 @@
 function treeSum(root) {
+    if(!root) return 0;
+    let sum = 0;
+    const nodeQueue = [root];
 
+    while(nodeQueue.length){
+        const node = nodeQueue.shift();
+        sum += node.val;
+
+        if(node.left) nodeQueue.push(node.left);
+        if(node.right) nodeQueue.push(node.right);
+    }
+    return sum;
 }
 
 
