620deb060e4ec27b2e1068a61be23c2e0d900f3c
yongbingao <ben.gao25@gmail.com>
2019-07-09 11:00:31 -0400

    Add solution to leet code problem

--- bst_project/lib/leet_code_110.js
+++ bst_project/lib/leet_code_110.js
@@ -3,5 +3,20 @@
 
 
 function isBalanced(root) {
+    if (root === null) return true;
 
+    const caclTree = (node) => {
+        if (node === null) return -1;
+
+        const left = caclTree(node.left) + 1;
+        const right = caclTree(node.right) + 1;
+
+        return Math.max(left, right);
+    }
+
+    const leftSide = caclTree(root.left);
+    const rightSide = caclTree(root.right);
+    if (Math.abs(leftSide - rightSide) > 1) return false;
+
+    return isBalanced(root.left) && isBalanced(root.right);
 }
