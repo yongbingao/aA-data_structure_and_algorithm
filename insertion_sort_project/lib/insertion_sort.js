9c2ada069fed3d9676decc551e996ae88710ac04
yongbingao <ben.gao25@gmail.com>
2019-09-14 16:47:28 -0400

    Add insert sort solution

--- insertion_sort_project/lib/insertion_sort.js
+++ insertion_sort_project/lib/insertion_sort.js
@@ -1,5 +1,15 @@
 function insertionSort(arr) {
+    for(let i = 1; i < arr.length; i++){
+        const currentVal = arr[i];
+        let j = i - 1;
+        for(j; j >= 0, currentVal < arr[j]; j--){
+            arr[j + 1] = arr[j];
+        }
 
+        arr[j + 1] = currentVal;
+    }
+
+    return arr;
 }
 
 module.exports = {
