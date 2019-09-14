50feb0e5f16d9775932ce3a4f3315241dc4feb0c
yongbingao <ben.gao25@gmail.com>
2019-09-14 16:34:12 -0400

    Add selection sort problem solution

--- selection_sort_project/lib/selection_sort.js
+++ selection_sort_project/lib/selection_sort.js
@@ -1,9 +1,20 @@
 function swap(arr, index1, index2) {
-
+    [arr[index1], arr[index2]] = [arr[index2], arr[index1]];
+    return arr;
 }
 
 function selectionSort(arr) {
+    for (let i = 0; i < arr.length; i++){
+        let minIndex = i;
 
+        for(let j = i + 1; j < arr.length; j++){
+            if (arr[j] < arr[minIndex]) minIndex = j;
+        }
+
+        swap(arr, i, minIndex);
+    }
+
+    return arr;
 }
 
 module.exports = {
