3718674b7690cf6976d08207caa3e187297afa44
yongbingao <ben.gao25@gmail.com>
2019-09-14 16:20:12 -0400

    Add bubble sort problem solutions

--- bubble_sort_project/lib/bubble_sort.js
+++ bubble_sort_project/lib/bubble_sort.js
@@ -1,9 +1,22 @@
 function swap(array, idx1, idx2) {
-
+    [array[idx1], array[idx2]] = [array[idx2], array[idx1]];
+    return array;
 }
 
 function bubbleSort(array) {
+    let swapped = true;
 
+    while (swapped){
+        swapped = false;
+        for(let i = 1; i < array.length; i++){
+            if (array[i] < array[i - 1]) {
+                swap(array, i, i - 1);
+                swapped = true;
+            }
+        }
+    }
+
+    return array;
 }
 
 
