e1343eb82f2864128d9a475c477b5f25bb3e686b
yongbingao <ben.gao25@gmail.com>
2019-09-16 16:46:00 -0400

    Add solution to counting sort

--- counting_sort_project/lib/counting_sort.js
+++ counting_sort_project/lib/counting_sort.js
@@ -1,5 +1,15 @@
 function countingSort(arr, max) {
+    const counterArr = new Array(max + 1,).fill(0);
+    const result = [];
 
+    arr.forEach(ele => counterArr[ele]++);
+    counterArr.forEach((ele, idx) => {
+        for(let i = 0; i < ele; i++){
+            result.push(idx);
+        }
+    })
+
+    return result;
 }
 
 
