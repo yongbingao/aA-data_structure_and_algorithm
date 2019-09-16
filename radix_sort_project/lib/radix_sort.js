3fd25a7d20597cf598c3fdf862ad4b5a8364b762
yongbingao <ben.gao25@gmail.com>
2019-09-16 15:38:10 -0400

    Add radix sort problem solution

--- radix_sort_project/lib/radix_sort.js
+++ radix_sort_project/lib/radix_sort.js
@@ -1,5 +1,40 @@
 function radixSort(arr) {
+    if(!Array.isArray(arr)) return null;
+    const maxDigit = getMaxDigits(arr);
+    let arrCopy = arr;
+    let i = 0;
 
+    while (i < maxDigit){
+        const buckets = [...Array(10)].map(() => Array());
+
+        arrCopy.forEach(ele => {
+            buckets[getDigitFrom(ele, i)].push(ele);
+        })
+
+        arrCopy = buckets.reduce((acc, current) => acc.concat(current), []);
+        i++;
+    }
+
+    return arrCopy;
+}
+
+function getDigitFrom(num, place){
+    return Math.floor(Math.abs(num) / 10**place) % 10;
+}
+
+function getIntLength(num){
+    return num === 0 ? 1 : Math.floor(Math.log(Math.abs(num))) + 1;
+}
+
+function getMaxDigits(nums){
+    let maxDigit = 0;
+
+    for(let i = 0; i < nums.length; i++){
+        const numDigit = getIntLength(nums[i]);
+        maxDigit = maxDigit < numDigit ? numDigit : maxDigit;
+    }
+
+    return maxDigit;
 }
 
 module.exports = {
