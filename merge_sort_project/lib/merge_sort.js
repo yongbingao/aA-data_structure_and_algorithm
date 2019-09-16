bb6e7971c5e28ff0a388734918213ebcf5918b57
yongbingao <ben.gao25@gmail.com>
2019-09-16 14:17:20 -0400

    Add merge sort problem solution

--- merge_sort_project/lib/merge_sort.js
+++ merge_sort_project/lib/merge_sort.js
@@ -1,9 +1,27 @@
 function merge(array1, array2) {
+    let result =[];
 
+    while(array1.length && array2.length){
+        if (array1[0] > array2[0]) result.push(array2.shift());
+        else result.push(array1.shift());
+    }
+
+    if (array1.length) result = result.concat(array1);
+    if (array2.length) result = result.concat(array2);
+
+    return result;
 }
 
 function mergeSort(array) {
+    if (array.length < 2) return array;
 
+    const len = array.length - 1;
+    const mid = Math.floor(array.length / 2);
+
+    const left = mergeSort(array.slice(0, mid));
+    const right = mergeSort(array.slice(mid));
+
+    return merge(left, right);
 }
 
 module.exports = {
